be7aba59577c163018f1950af09ada1b5d701a75
ForceWithin <davisnick2012@gmail.com>
2023-04-01 19:02:31 +0300

    Update pull_request3.php

--- pull_request3.php
+++ pull_request3.php
@@ -1,16 +1,85 @@
 <?php
-class HomeController extends AbstractController
+
+class QuestionController extends AbstractController
 {
+    private $logger;
+    private $isDebug;
+    public function __construct(LoggerInterface $logger, bool $isDebug)
+    {
+        $this->logger = $logger;
+        $this->isDebug = $isDebug;
+    }
     /**
      * @Route("/", name="app_homepage")
      */
     public function homepage(EntityManagerInterface $entityManager)
     {
+        $this->updateUserVisitCounter($entityManager);
+        $repository = $entityManager->getRepository(Question::class);
+        $questions = $repository->findAll();
+        dd($questions);
+        return $this->render('question/homepage.html.twig');
+    }
+    /**
+     * @Route("/questions/new")
+     */
+    public function new(EntityManagerInterface $entityManager)
+    {
+        $this->updateUserVisitCounter($entityManager);
+        $question = new Question();
+        $question->setName('Missing pants')
+            ->setSlug('missing-pants-'.rand(0, 1000))
+            ->setQuestion(<<<EOF
+Hi! So... I'm having a *weird* day. Yesterday, I cast a spell
+to make my dishes wash themselves. But while I was casting it,
+I slipped a little and I think `I also hit my pants with the spell`.
+When I woke up this morning, I caught a quick glimpse of my pants
+opening the front door and walking out! I've been out all afternoon
+(with no pants mind you) searching for them.
+Does anyone have a spell to call your pants back?
+EOF
+            );
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+        $entityManager->persist($question);
+        $entityManager->flush();
+        return new Response(sprintf(
+            'Well hallo! The shiny new question is id #%d, slug: %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
+    }
+    /**
+     * @Route("/questions/{slug}", name="app_question_show")
+     */
+    public function show($slug, EntityManagerInterface $entityManager)
+    {
+        $this->updateUserVisitCounter($entityManager);
+        if ($this->isDebug) {
+            $this->logger->info('We are in debug mode!');
+        }
+        $repository = $entityManager->getRepository(Question::class);
+        /** @var Question|null $question */
+        $question = $repository->findOneBy(['slug' => $slug]);
+        if (!$question) {
+            throw $this->createNotFoundException(sprintf('no question found for slug "%s"', $slug));
+        }
+        $answers = [
+            'Make sure your cat is sitting `purrrfectly` still ?',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe... try saying the spell backwards?',
+        ];
+        return $this->render('question/show.html.twig', [
+            'question' => $question,
+            'answers' => $answers,
+        ]);
+    }
+    
+    private function updateUserVisitCounter($entityManager) {
         $user = $this->tokenStorage()->getToken()->getUser();
         $user->updateCounter();
         $entityManager->persist($user);
         $entityManager->flush();
-        
-        return $this->render('page/homepage.html.twig');
     }
 }
