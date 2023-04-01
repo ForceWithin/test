c51b447dff470d8f4977a68b4d322bc418ab5339
ForceWithin <davisnick2012@gmail.com>
2023-04-01 18:14:10 +0300

    Create optimize2.php

---
+++ optimize2.php
@@ -0,0 +1,40 @@
+class CarController extends AbstractController
+{
+
+    public function getCarByName($name) {
+      $cars = $entityManager->getRepository(Car::class)->findAll();
+
+      foreach($cars as $car) {
+        if ($car->getName() == $name) {
+            return $car;
+         }
+      }
+      
+      return null;
+    }
+
+    public function getCarByEngine($engine) {
+      $cars = $entityManager->getRepository(Car::class)->findAll();
+
+      foreach($cars as $car) {
+        if ($car->getEngine() == $engine) {
+            return $car;
+         }
+      }
+      
+      return null;
+    }
+
+    public function getCarByColor($color) {
+      $cars = $entityManager->getRepository(Car::class)->findAll();
+
+      foreach($cars as $car) {
+        if ($car->getColor() == $color) {
+            return $car;
+         }
+      }
+      
+      return null;
+    }
+   
+}
